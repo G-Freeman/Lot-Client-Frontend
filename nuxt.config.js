c862286f4d574125f98630b35c4d92604ca16de7
Dr.Freeman <Dr.Zukis@rambler.ru>
2021-10-17 00:27:20 +0300

    Docker init

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,38 +21,36 @@ export default {
 	// Global CSS: https://go.nuxtjs.dev/config-css
 	css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-  ],
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		// https://go.nuxtjs.dev/typescript
+		'@nuxt/typescript-build',
+	],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-  ],
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+		// https://go.nuxtjs.dev/axios
+		'@nuxtjs/axios',
+		// https://go.nuxtjs.dev/pwa
+		'@nuxtjs/pwa',
+	],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+	// Axios module configuration: https://go.nuxtjs.dev/config-axios
+	axios: {},
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: 'en'
-    }
-  },
+	// PWA module configuration: https://go.nuxtjs.dev/pwa
+	pwa: {
+		manifest: {
+			lang: 'en'
+		}
+	},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {},
 }
